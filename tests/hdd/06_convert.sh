2d976453565479a2c9d295b7ced1199465bb3814
He Xingchen <chuanjun1978@gmail.com>
2026-06-22 10:17:02 +0800

    add HDD convert methods

---
+++ tests/hdd/06_convert.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# RAW -> ERWT3D 转换
+
+input = /mnt/f/zhoujiawang/CUP/cup_3d_small.dat
+output = /mnt/f/zhoujiawang/CUP/cup_3d_small.erwt3d
+
+erwt3d begin convert
+    erwt3d convert input=$input output=$output nx=801 ny=2405 nz=2501
+erwt3d end
